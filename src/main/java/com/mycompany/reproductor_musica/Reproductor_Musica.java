f279a6559ba635d517ee47e2439171b6f50ec230
AdrianMO290922 <adrian.martinez.260821@gmail.com>
2024-02-05 08:27:44 -0600

    Funciones basicas

---
+++ src/main/java/com/mycompany/reproductor_musica/Reproductor_Musica.java
@@ -0,0 +1,13 @@
+
+
+package com.mycompany.reproductor_musica;
+
+
+public class Reproductor_Musica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Repro audio = new Repro();
+        audio.setVisible(true);
+    }
+}
